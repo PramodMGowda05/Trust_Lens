9eead94df73653bf650191dbb53360ac14a1b17c
Pramod Gowda <pramodgowda3311@gmail.com>
2025-11-10 16:54:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-709951747-1e89b",
+  "appId": "1:926845549555:web:7782a6b565d64764aab086",
+  "apiKey": "AIzaSyBTb2e2kBE3zFzEG7zEMVIN6_GPYsdyDXg",
+  "authDomain": "studio-709951747-1e89b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "926845549555"
+};
