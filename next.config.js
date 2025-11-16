54cec7da04d4a606f3d433d72ba6c09a423893d7
Pramod Gowda <pramodgowda3311@gmail.com>
2025-11-16 17:17:54 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-import type {NextConfig} from 'next';
 require('dotenv').config({ path: './.env' });
 
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -34,4 +33,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
